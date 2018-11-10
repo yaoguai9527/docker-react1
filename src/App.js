b7847a3942615d754d5160fece76e525655cc4e0
Chuang Huang <chuang.huang12@gmail.com>
2018-11-10 17:42:32 +0800

    something new

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Changed to something else
+            This is something new.
           </p>
           <a
             className="App-link"
